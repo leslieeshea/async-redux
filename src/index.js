bcc8dd9653089f67d85541c8813b8eacf14602ea
Leslie Shea <leslieyshea@gmail.com>
2019-05-29 14:16:14 -0700

    tracking change in store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
 
-render(
-  <App />,
-  document.getElementById('root')
+const logger = store => next => action => {
+  // before the reducer has changed state
+  console.log('Before reducer', store.getState());
+
+  // go to the next middleware
+  next(action);
+
+  // after the reducer has changed state
+  console.log('After reducer', store.getState());
+};
+
+function reducer(state = {}, action) {
+  switch(action.type) {
+    case 'HI':
+      return 'hi there!';
+    default:
+      return state;
+  }
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger)
 );
+
+store.dispatch({
+  type: 'HI'
+});
