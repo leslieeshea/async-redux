f62c7d66606d71e9ba241ee9e754489fa1fa8f63
Leslie Shea <leslieyshea@gmail.com>
2019-05-30 10:02:24 -0700

    created character actions to fetch characters

---
+++ src/actions/characterActions.js
@@ -0,0 +1,25 @@
+import { getCharacters } from '../services/avatarApi';
+
+export const FETCH_CHARACTERS_LOADING = 'FETCH_CHARACTERS_LOADING';
+export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
+export const FETCH_CHARACTERS_ERROR = 'FETCH_CHARACTERS_ERROR';
+
+export const fetchCharacters = () => dispatch => {
+  dispatch({
+    type: FETCH_CHARACTERS_LOADING
+  });
+
+  return getCharacters()
+    .then(character => {
+      dispatch({
+        type: FETCH_CHARACTERS,
+        payload: character
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: FETCH_CHARACTERS_ERROR,
+        payload: err
+      });
+    });
+};
