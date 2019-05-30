ea79ce60a230280eb3e713fa4a93bb6362bfbc57
Leslie Shea <leslieyshea@gmail.com>
2019-05-30 09:43:17 -0700

    created characters list

---
+++ src/components/characters/Characters.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Character from './Character';
+
+function Characters({ characters }) {
+  const characterList = characters.map(character => (
+    <li key={character.id}>
+      <Character character={character} />
+    </li>
+  ));
+
+  return (
+    <ul>
+      {characterList}
+    </ul>
+  );
+}
+
+Characters.propTypes = {
+  characters: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default Characters;
